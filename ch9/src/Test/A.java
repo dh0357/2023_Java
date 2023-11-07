cb0e3b9f5eb1e8df7e809d182400e079d02b3784
dh0357 <147688951+dh0357@users.noreply.github.com>
2023-11-07 10:41:11 +0900

    Add files via upload
    자바 9장 실습 코드 파일

---
+++ ch9/src/Test/A.java
@@ -0,0 +1,55 @@
+package Test;
+
+public class A {
+	static int memberA = 10;
+	int field1 = 10;
+	static int field2 = 20;
+	B b = new B();
+	C c = new C();
+	static B b1 = new B();
+	static C c1 = new c();
+	
+	
+	
+	void method1() {}
+	static void method2() {}
+	
+	A(){
+		System.out.println("A instance is generated.");
+	}
+	
+	 class B {
+		 void method() {
+			 field1 = 30;
+			 method1();
+			 field2 = 40;
+			 method2();
+		 }
+	 }
+		
+		}
+	
+	static class C {
+		 void method() {
+			 //field1 = 30;
+			 //method1();
+			 field2 = 40;
+			 method2();
+		
+	}
+	
+	
+	void method() {
+		class D {
+			D(){
+				System.out.println("D instance is generated.");
+			}
+		}
+		D d = new D();
+	}
+	
+	void method2() {
+		
+	}
+}
+
---
+++ ch9/src/Test/Main.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A a = new A();
+		A.B b = a.new A.B();
+		A.memberA = 20;
+	}
+
+}
